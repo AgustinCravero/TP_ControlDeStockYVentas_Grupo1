2a4ea0c0b233b5ae8326793ed72f15fd5cdf85b7
tomas7L <buffatomy06@gmail.com>
2025-10-08 20:26:24 -0300

    Conexion a la base de datos

---
+++ ClasesTP/Data/AplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClasesTP.Models;
+
+namespace ClasesTP.Data
+{
+    public class AplicationDbContext : DbContext
+    {
+        public DbSet<Venta> Ventas { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server=localhost;Database=TPWindowsForms;Trusted_Connection=True;TrustServerCertificate=True;"
+            );
+        }
+    }
+}
---
+++ ClasesTP/Models/Venta.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasesTP.Models
+{
+    public class Venta
+    {
+        public int Id { get; set; }
+        public DateTime Fecha { get; set; }
+    }
+}
